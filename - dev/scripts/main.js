06611c22d27a86a850d8b68a16e27764f8e9ca04
yeffasol <yeffasol@mail.ru>
2016-07-18 22:46:08 +0300

    rating сделан с помощью js

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -34,15 +34,62 @@ var SliderWidjet = (function () {
                     }
                 });
             });
-
-            //     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-//         " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-// } );
         }
     }
 }());
 
+var RatingWidjet = (function () {
+    
+    var _letTheStarsShining = function (ratingAmount) {
+        var
+            starsArray = [];
+
+        for(var i=0; i<5; i++) {
+            var
+                starClassName = (i < ratingAmount) ? "products__rating-stars-item  products__rating-stars-item_filled" : "products__rating-stars-item";
+
+            var star = $("<li>", {
+                class : starClassName
+            });
+
+            starsArray.push(star);
+        }
+
+        return starsArray;
+    }
+    var _generateMarkup = function(ratingAmount, elementToAppend){
+        var
+            ul = $("<ul>",{
+                class: "products__rating-stars",
+                html : _letTheStarsShining(ratingAmount)
+            });
+
+        var
+            ratingDispdlay = $("<div>", {
+                class : "products__rating-amount",
+                text : ratingAmount
+            });
+
+        elementToAppend.append([ul, ratingDispdlay]);
+    }
+    return {
+        init: function () {
+            $(".products__rating").each(function () {
+                var
+                    $this = $(this),
+                    ratingAmount = parseInt($this.data("rating"));
+
+                _generateMarkup(ratingAmount, $this);
+            });
+        }
+    }
+}());
 $(document).ready(function () {
+
+    if($(".products__rating").length) {
+        RatingWidjet.init();
+    }
+
     if($(".filter__slider-element").length) {
         SliderWidjet.init();
     }
